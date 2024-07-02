017e64ac8d73cdf27954c2b8ba5bee3d72ef24cc
seunghyonpark <creath.park@gmail.com>
2024-07-02 19:18:24 +0900

    good

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -122,7 +122,14 @@ export default function Pricing({ user, products, subscription }: Props) {
 
             <button
               disabled={!user}
-              onClick={() => window.open('http://store.unove.space/Store/depositpopup?storecode=2000002&memberid='+user?.email, '_blank')}
+
+              //onClick={() => window.open('http://store.unove.space/Store/depositpopup?storecode=2000002&memberid='+user?.email, '_blank')}
+              
+
+              // https://corky.unove.space/payment?storecode=2000002&memberid=
+
+              onClick={() => window.open('https://corky.vercel.app/payment?storecode=2000002&memberid='+user?.email, '_blank')}
+
               type="button"
               className={`relative w-1/2 bg-pink-500 border-pink-500 shadow-sm text-white rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
             >
